ef54f6b44d07043b7fa3502bf4790c9ef9b5b1a8
seraphy <seraphy@users.osdn.me>
2018-09-12 11:33:01 +0900

    バッチファイル修正

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
+cd /d %~dp0
 call setenv.cmd
-if exist bin rmdir /s /q bin
 call mvn clean package
 java --module-path app\target\mods -m jp.seraphyware.java9example1app/jp.seraphyware.java9example1app.Main
 if "%1" == "" pause
